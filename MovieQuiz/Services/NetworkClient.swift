0438c73318f50e8aa5b457e85d9f4a65f23eb6cc
IrinaBobarykina <ira.bobarykina5@gmail.com>
2024-01-08 11:12:38 +0100

    Added unit tests for MovieLoader

--- MovieQuiz/Services/NetworkClient.swift
+++ MovieQuiz/Services/NetworkClient.swift
@@ -1,7 +1,11 @@
 /// Отвечает за загрузку данных по URL
 import Foundation
 
-struct NetworkClient {
+protocol NetworkRouting {
+    func fetch(url: URL, handler: @escaping (Result<Data, Error>) -> Void)
+}
+
+struct NetworkClient: NetworkRouting {
 
     private enum NetworkError: Error {
         case codeError
