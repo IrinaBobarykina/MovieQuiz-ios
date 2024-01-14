2476e9c61b6e8b9e143bf593e2b4314635dd2552
IrinaBobarykina <ira.bobarykina5@gmail.com>
2024-01-14 15:08:35 +0100

    Added a test for presenter

---
+++ MovieQuiz/Services/MovieQuizViewControllerProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  MovieQuizViewControllerProtocol.swift
+//  MovieQuiz
+//
+//  Created by Ирина Бобарыкина on 14.01.2024.
+//
+
+import Foundation
---
+++ MovieQuizTests/MovieQuizPresenterTests.swift
@@ -0,0 +1,8 @@
+//
+//  MovieQuizPresenterTests.swift
+//  MovieQuizTests
+//
+//  Created by Ирина Бобарыкина on 14.01.2024.
+//
+
+import Foundation
