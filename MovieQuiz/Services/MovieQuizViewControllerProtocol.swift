bb599aa52632221c92b9f545e562644f8eafee7d
IrinaBobarykina <ira.bobarykina5@gmail.com>
2024-01-14 15:10:44 +0100

    Adeed a test for Presenter v.2.0

--- MovieQuiz/Services/MovieQuizViewControllerProtocol.swift
+++ MovieQuiz/Services/MovieQuizViewControllerProtocol.swift
@@ -1,8 +1,15 @@
-//
-//  MovieQuizViewControllerProtocol.swift
-//  MovieQuiz
-//
-//  Created by Ирина Бобарыкина on 14.01.2024.
-//
 
 import Foundation
+
+protocol MovieQuizViewControllerProtocol: AnyObject {
+    func show(quiz step: QuizStepViewModel)
+    func show(quiz result: QuizResultsViewModel)
+    
+    func highlightImageBorder(isCorrectAnswer: Bool)
+    
+    func showLoadingIndicator()
+    func hideLoadingIndicator()
+    
+    func showNetworkError(message: String)
+}
+
